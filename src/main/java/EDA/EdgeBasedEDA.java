dba40f3b823341915d2d0b7ea309862b6733a4e0
riedersa <rieder.sab@outlook.de>
2020-05-07 18:40:39 +0200

    Added second EDA

--- src/main/java/EDA/EdgeBasedEDA.java
+++ src/main/java/EDA/EdgeBasedEDA.java
@@ -4,36 +4,35 @@ import DataStructures.Graph;
 import DataStructures.TSPTour;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.Random;
 
 /**
- * This class performs UMDA. The algorithm stops, after a specified number of iterations. The implementation is based on
- * "XU, Zhe, et al. Immune algorithm combined with estimation of distribution for traveling salesman problem. IEEJ
- * Transactions on Electrical and Electronic Engineering, 2016, 11. Jg., S. S142-S154.".
- * <p>
- * TODO: add another stopping criterion like no improvement
+ * This class is an edge based EDA. It can only be used for symmetric TSP. It is based on "TSUTSUI, Shigeyoshi.
+ * Probabilistic model-building genetic algorithms in permutation representation domain using edge histogram. In:
+ * International Conference on Parallel Problem Solving from Nature. Springer, Berlin, Heidelberg, 2002. S. 224-233".
  * <p>
  * TODO: Tests
+ * <p>
+ * TODO: stopping criterion
  */
-public class PositionBasedEDA_UMDA implements EDA {
+public class EdgeBasedEDA implements EDA {
 
-    /* This is the probabilistic model used by the EDA. model[i][j] is the probability that node i is at position j
-    of the tour*/
-    private double[][] model;
+    private double[][] edgeHistogramMatrix;
     private int selectedPopulationSize;
     private int sampledPopulationSize;
     private final int numberNodes;
     private int maxCounterOtIterations;
 
     private double probForPriorTour = 0.2;
+    private double bRatio = 0.15; //Used to create the noise. If b is high, then the perturbation is high.
+    private double epsilon; //noise for the matrix according to the paper
 
     private final Graph graph;
 
 
     /**
-     * This method creates a new instance of an UMDA.
+     * This method creates a new instance of an EDA.
      *
      * @param graph                  the graph on which the TSP instance is based.
      * @param numberNodes            the number of nodes the current TSP instance has.
@@ -43,8 +42,8 @@ public class PositionBasedEDA_UMDA implements EDA {
      * @throws IllegalArgumentException Throws an IllegalArgumentException if the sampledPopulationSize is smaller thant
      *                                  the selectedPopulationSize.
      */
-    public PositionBasedEDA_UMDA(Graph graph, int numberNodes, int selectedPopulationSize, int sampledPopulationSize,
-                                 int maxCounterOtIterations) throws IllegalArgumentException {
+    public EdgeBasedEDA(Graph graph, int numberNodes, int selectedPopulationSize, int sampledPopulationSize,
+                        int maxCounterOtIterations) throws IllegalArgumentException {
         if (sampledPopulationSize < selectedPopulationSize) {
             throw new IllegalArgumentException(String.format("The size of the population after sampling should be " +
                     "bigger than the size after selecting the best individuals. The sizes were %d for sampling and %d" +
@@ -52,63 +51,28 @@ public class PositionBasedEDA_UMDA implements EDA {
         }
         this.graph = graph;
         this.numberNodes = numberNodes;
-        this.model = new double[numberNodes][numberNodes];
+        this.edgeHistogramMatrix = new double[numberNodes][numberNodes];
         this.selectedPopulationSize = selectedPopulationSize;
         this.sampledPopulationSize = sampledPopulationSize;
         this.maxCounterOtIterations = maxCounterOtIterations;
+        setEpsilon();
     }
 
 
     @Override
     public TSPTour perturb(TSPTour tour) {
-        initiateModel(tour);
-        return performEDA();
+        return null;
     }
 
 
     @Override
     public TSPTour initiate() {
-        initiateModel();
-        return performEDA();
+        return null;
     }
 
 
     /**
-     * Initiates the model if there is no prior knowledge. Then each node is equally likely at each position.
-     */
-    private void initiateModel() {
-        double prob = 1.0 / numberNodes;
-        for (int node = 0; node < numberNodes; node++) {
-            for (int position = 0; position < numberNodes; node++) {
-                model[node][position] = prob;
-            }
-        }
-    }
-
-
-    /**
-     * Initiates the model if there is one tour given. Every node gets {@code probForPriorTour} probability for the
-     * position it is assigned in the given tour.
-     *
-     * @param tspTour the given tour
-     */
-    private void initiateModel(TSPTour tspTour) {
-        double prob = (1.0 - probForPriorTour) / (numberNodes - 1.0);
-        for (int node = 0; node < numberNodes; node++) {
-            for (int position = 0; position < numberNodes; node++) {
-                model[node][position] = prob;
-            }
-        }
-        int[] tour = tspTour.getTour();
-        for (int position = 0; position < numberNodes; position++) {
-            model[tour[position]][position] = probForPriorTour;
-        }
-    }
-
-
-    /**
-     * This method performs the EDA and returns the best value found. It uses the model to sample the first population,
-     * so make sure, that the model was updated before calling this method. The constructor does not do this!
+     * This method performs an EDA. Notice, that before invoking this method, the edge histogram matrix must be filled.
      *
      * @return the best tour found
      */
@@ -144,127 +108,103 @@ public class PositionBasedEDA_UMDA implements EDA {
 
 
     /**
-     * This methods updates the probability model.
+     * This method creates the edge histogram matrix. It can only be used for symmetric TSPs.
      *
-     * @param tspTours the current population that shoulc be used to update the model.
+     * @param tspTours the population for which to create the matrix
      */
     private void estimate(PriorityQueue<TSPTour> tspTours) {
-        int populationSize = tspTours.size();
-
-        int[][] numberOccurrencesAtPosition = new int[numberNodes][numberNodes];
-
+        setEpsilon();
+        //empty the matrix to fill it later again via just addition
+        for (int i = 0; i < numberNodes; i++) {
+            for (int j = 0; j < numberNodes; j++) {
+                if (i != j) {
+                    edgeHistogramMatrix[i][j] = epsilon;
+                } else {
+                    edgeHistogramMatrix[i][j] = 0;
+                }
+            }
+        }
         for (TSPTour tspTour : tspTours) {
             int[] tour = tspTour.getTour();
             for (int position = 0; position < numberNodes; position++) {
-                numberOccurrencesAtPosition[tour[position]][position]++;
-            }
-        }
-
-        double noiseNumerator = 1.0 / numberNodes;
-        double denominator = populationSize + (((double) populationSize) / numberNodes);
-
-        for (int node = 0; node < numberNodes; node++) {
-            for (int position = 0; position < numberNodes; position++) {
-                model[node][position] = (numberOccurrencesAtPosition[node][position] + noiseNumerator) / denominator;
+                edgeHistogramMatrix[tour[position]][tour[(position + 1) % numberNodes]]++;
+                edgeHistogramMatrix[tour[(position + 1) % numberNodes]][tour[position]]++;
             }
         }
     }
 
 
     /**
-     * This method creates new solutions according to the probability model and adds it to the tspTours.
+     * Samples new individuals for the population until there are {@code sampledPopulationSize} individuals in the
+     * population.
      *
-     * @param sizeOfPopulationAfterSampling the size, the population should have after sampling
-     * @param tspTours                      the current population
+     * @param tspTours the current population. It is extended by new individuals.
      */
-    private void sample(int sizeOfPopulationAfterSampling, PriorityQueue<TSPTour> tspTours) {
-        while (tspTours.size() < sizeOfPopulationAfterSampling) {
-            int[] tourArray = rouletteWheelForCreation();
-            tourArray = refineTour(tourArray);
-            TSPTour tour = new TSPTour(tourArray);
-            graph.setDistanceToTour(tour);
-            tspTours.add(tour);
+    private void sample(int sampledPopulationSize, PriorityQueue<TSPTour> tspTours) {
+        while (tspTours.size() < sampledPopulationSize) {
+            tspTours.add(createTour());
         }
     }
 
 
     /**
-     * Returns a tour, that was sampled using the roulette wheel technique and the model. Notice, that this tour must
-     * not be feasible.
+     * This method creates a new tour according to the edge histogram matrix.
      *
-     * @return the generated Tour. This must not be feasible.
+     * @return the newly generated tour.
      */
-    private int[] rouletteWheelForCreation() {
+    private TSPTour createTour() {
+        Random randomGenerator = new Random();
         int[] tour = new int[numberNodes];
-        Random randomGenerator = new Random();
-        for (int position = 0; position < numberNodes; position++) {
-            double randomNumber = randomGenerator.nextDouble();
+        int positionCounter = 0;//The next position to be filled
+
+        tour[0] = randomGenerator.nextInt(numberNodes);
+        positionCounter++;
+
+        for (int position = positionCounter; position < numberNodes; position++) {
+            double[] vector = rouletteWheelVector(tour, positionCounter);
+            double random = randomGenerator.nextDouble();
             for (int node = 0; node < numberNodes; node++) {
-                if (randomNumber <= model[node][position]) {
-                    tour[position] = node;
+                if (vector[node] > 0 && random <= vector[node]) {
+                    tour[positionCounter] = node;
+                    positionCounter++;
                     break;
-                } else {
-                    randomNumber -= model[node][position];
                 }
             }
         }
-        return tour;
+
+        TSPTour tspTour = new TSPTour(tour);
+        graph.setDistanceToTour(tspTour);
+        return tspTour;
     }
 
 
     /**
-     * Takes an possibly infeasible tour and makes it feasible.
+     * Constructs the roulette wheel vector for the tour, when the next position to fill is {@code counter}.
      *
-     * @param tour the tour to change
-     * @return the feasible tour based on the given one
+     * @param tour    the current tour
+     * @param counter the next position to fill
+     * @return
      */
-    private int[] refineTour(int[] tour) {
-        //Is true, if node i is already found in the tour.
-        boolean[] alreadyFound = new boolean[numberNodes];
-
-        HashSet<Integer> missingNodes = new HashSet<>();
-
-        HashSet<Integer> doubleLocations = new HashSet<>();
-
-        Random randomGenerator = new Random();
-
+    private double[] rouletteWheelVector(int[] tour, int counter) {
+        double[] vector = edgeHistogramMatrix[tour[counter - 1]].clone();
+        for (int i = 0; i < counter; i++) {
+            vector[tour[i]] = 0;
+        }
+        double sum = 0;
         for (int i = 0; i < numberNodes; i++) {
-            if (alreadyFound[tour[i]]) {
-                doubleLocations.add(i);
-            }
+            sum += vector[i];
         }
-
         for (int i = 0; i < numberNodes; i++) {
-            if (!alreadyFound[i]) {
-                missingNodes.add(i);
-            }
+            vector[i] = vector[i] / sum;
         }
-
-
-        for (Integer position : doubleLocations) {
-            //this is the distance between pos-1 and the node plus pos+1 and the node.
-            long[] distanceAddedIfNode = new long[numberNodes];
-            //the distances summed up
-            double overallDistance = 0;
-
-            for (Integer node : missingNodes) {
-                distanceAddedIfNode[node] = graph.getDistance(tour[(position - 1) % numberNodes], node)
-                        + graph.getDistance(node, tour[(position + 1) % numberNodes]);
-                overallDistance += distanceAddedIfNode[node];
-            }
-
-            double random = randomGenerator.nextDouble();
-
-            for (int node : missingNodes) {
-                if (random <= ((double) distanceAddedIfNode[node]) / overallDistance) {
-                    missingNodes.remove(node);
-                    tour[position] = node;
-                }
-            }
-        }
-
-        return tour;
+        return vector;
     }
+
+
+    /*
+     ----------------------------------------------------------------------------------------------------------------------
+     Getter and Setter
+     */
 
 
     public int getSelectedPopulationSize() {
@@ -284,6 +224,7 @@ public class PositionBasedEDA_UMDA implements EDA {
                     " for selecting.", sampledPopulationSize, selectedPopulationSize));
         }
         this.selectedPopulationSize = selectedPopulationSize;
+        setEpsilon();
     }
 
 
@@ -333,5 +274,21 @@ public class PositionBasedEDA_UMDA implements EDA {
             throw new IllegalArgumentException(String.format("The probability is too large: %e", probForPriorTour));
         }
         this.probForPriorTour = probForPriorTour;
+    }
+
+
+    public double getbRatio() {
+        return bRatio;
+    }
+
+
+    public void setbRatio(double bRatio) {
+        this.bRatio = bRatio;
+        setEpsilon();
+    }
+
+
+    private void setEpsilon() {
+        this.epsilon = (2.0 * selectedPopulationSize) / (numberNodes - 1) * bRatio;
     }
 }
